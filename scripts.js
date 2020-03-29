a1781d11dce5fb5fc59768898db4a0675ae60997
GeovanneAtanazio <geovanne.atanazio@dcomp.ufs.br>
2020-03-29 07:21:32 -0300

    Criação da página ideas

--- scripts.js
+++ scripts.js
@@ -3,5 +3,14 @@ function onOFF() {
             .querySelector('#modal')
             .classList
             .toggle('hide')
+
+    document
+            .querySelector('body')
+            .classList
+            .toggle('hideScroll')
+    document
+            .querySelector('#modal')
+            .classList
+            .toggle('addScroll')
     
 }
